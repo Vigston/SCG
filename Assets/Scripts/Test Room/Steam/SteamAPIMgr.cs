6ab1aad0b0fed440806a3dac3bfec9e114e79be0
あまおう <58240927+Amaou0014@users.noreply.github.com>
2025-04-10 17:57:17 +0900

    SteamAPIの実行環境構築

---
+++ Assets/Scripts/Test Room/Steam/SteamAPIMgr.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Steamworks;
+
+public class SteamAPIMgr : MonoBehaviour
+{
+    void Start()
+    {
+        if(SteamAPI.Init())
+        {
+			Debug.Log("SteamAPI Initialized");
+		}
+		else
+		{
+			Debug.LogError("SteamAPI initialization failed");
+		}
+	}
+
+	private void OnApplicationQuit()
+	{
+		SteamAPI.Shutdown();
+	}
+}
