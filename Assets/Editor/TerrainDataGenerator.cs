f20880d8a87cf99cfd2ecb035e5747dd637fa4c9
あまおう <58240927+Amaou0014@users.noreply.github.com>
2025-06-10 18:12:18 +0900

    Stage制作(途中)

---
+++ Assets/Editor/TerrainDataGenerator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public static class TerrainDataGenerator
+{
+	[MenuItem("Assets/Create/TerrainData")]
+	public static void CreateTerrainData()
+	{
+		// インスタンスを作成
+		TerrainData data = new TerrainData();
+
+		// ファイル名入力ダイアログを表示して保存
+		string defaultName = "New TerrainData.asset";
+		string path = AssetDatabase.GenerateUniqueAssetPath("Assets/Terrain/" + defaultName);
+
+		// CreateAssetWithSavePrompt風の動き
+		ProjectWindowUtil.CreateAsset(data, path);
+	}
+}
