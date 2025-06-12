c41b431b2c1883538e84740f9f7b76c7c8160466
あまおう <58240927+Amaou0014@users.noreply.github.com>
2025-06-12 15:43:57 +0900

    ステージ生成処理作成。(細かな調整は必要)
    オブジェクトの配置などで動きのあるステージにしたい。Terrainで実装しているので高低差も作れるがモバイルでの描画不可を考えた実装が必要。

---
+++ Assets/Editor/TerrainTexturePainterEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(TerrainTexturePainter))]
+public class TerrainTexturePainterEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+
+		TerrainTexturePainter painter = (TerrainTexturePainter)target;
+
+		if (GUILayout.Button("範囲内にテクスチャを塗る"))
+		{
+			painter.PaintTextureInBox();
+		}
+	}
+}
---
+++ Assets/Scripts/Stage/TerrainTexturePainter.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode] // エディター上でも動くようにする
+public class TerrainTexturePainter : MonoBehaviour
+{
+	public Terrain terrain;
+	public int targetTextureIndex = 1;
+
+	// 公開メソッド（Editorから呼び出せる）
+	public void PaintTextureInBox()
+	{
+		BoxCollider box = GetComponent<BoxCollider>();
+		if (terrain == null || box == null)
+		{
+			Debug.LogWarning("TerrainまたはBoxColliderが見つかりません");
+			return;
+		}
+
+		TerrainData terrainData = terrain.terrainData;
+		Vector3 terrainPosition = terrain.transform.position;
+		int mapWidth = terrainData.alphamapWidth;
+		int mapHeight = terrainData.alphamapHeight;
+
+		float[,,] alphas = terrainData.GetAlphamaps(0, 0, mapWidth, mapHeight);
+
+		for (int y = 0; y < mapHeight; y++)
+		{
+			for (int x = 0; x < mapWidth; x++)
+			{
+				float worldX = terrainPosition.x + ((float)x / mapWidth) * terrainData.size.x;
+				float worldZ = terrainPosition.z + ((float)y / mapHeight) * terrainData.size.z;
+				Vector3 worldPos = new Vector3(worldX, 0, worldZ);
+
+				if (box.bounds.Contains(worldPos))
+				{
+					for (int i = 0; i < terrainData.alphamapLayers; i++)
+					{
+						alphas[y, x, i] = (i == targetTextureIndex) ? 1 : 0;
+					}
+				}
+			}
+		}
+
+		terrainData.SetAlphamaps(0, 0, alphas);
+		Debug.Log("テクスチャをBoxCollider内に塗りました！");
+	}
+}
